67f989000fcc3a8a803d45471c51494b387a4e67
eduardoVela <eduardovela2022@icloud.com>
2024-06-17 14:08:13 -0600

    Added Product model.

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,60 @@
-// import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
-const sequelize = require('../config/connection');
+// Imports
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+// Category class inherits from the Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Product class (Model)
 Product.init(
   {
-    // define columns
+    // Product id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Product name
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // Product price
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    // Product stock
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    // Product category
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
+    // Model settings
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
+// Exports
 module.exports = Product;
