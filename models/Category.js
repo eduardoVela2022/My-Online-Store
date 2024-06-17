c757467c69d161a36cf04812f66a65faef8a44b1
eduardoVela <eduardovela2022@icloud.com>
2024-06-17 13:52:55 -0600

    Added Category model.

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,35 @@
-const { Model, DataTypes } = require('sequelize');
+// Imports
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
 
-const sequelize = require('../config/connection.js');
-
+// Category class inherits from the Model class
 class Category extends Model {}
 
+// Category class (Model)
 Category.init(
   {
-    // define columns
+    // Category id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Category name
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
+    // Model settings
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
+// Exports
 module.exports = Category;
